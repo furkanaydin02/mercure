dc0deda3e49031074af2290e95272bec06d35ba8
furkan.aydin <furkan.aydin@enuygun.com>
2023-12-06 16:12:04 +0300

    *

---
+++ src/Controller/MercureController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Mercure\HubInterface;
+use Symfony\Component\Mercure\Update;
+use Symfony\Component\Routing\Annotation\Route;
+
+class MercureController extends AbstractController
+{
+    #[Route('/home',  name: 'home')]
+    public function index()
+    {
+        return $this->render('index.html.twig');
+    }
+
+    #[Route('/push',  name: 'push')]
+    public function publish(HubInterface $hub): Response
+    {
+        $update = new Update(
+            'https://example.com/books/1',
+            json_encode(['status' => 'OutOfStock'])
+        );
+
+        $hub->publish($update);
+
+        return new Response('published!');
+    }
+}
